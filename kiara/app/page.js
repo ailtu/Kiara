d8263c8e6abbff8f26d9836f602f05789722fa7c
Ailton Cesar <ailtonc.n64@gmail.com>
2025-06-16 02:21:09 +0000

    Adição de texto "Em desenvolvimento"

--- kiara/app/page.js
+++ kiara/app/page.js
@@ -3,19 +3,27 @@ import kiara from '../public/logo/logo_kiara.png'
 
 export default function Home() {
   return (
-     <div style={{
+    <div style={{
       display: 'flex',
+      flexDirection: 'column',
       justifyContent: 'center',
       alignItems: 'center',
       height: '100vh',
       backgroundColor: '#000000'
     }}>
-      <Image 
+      <Image
         src={kiara}
         alt="Logo da Kiara"
         width={300}
         height={400}
       />
+      <p style={{
+        color: '#ffffff',
+        marginTop: '20px',
+        fontSize: '18px',
+      }}>
+        Em desenvolvimento...
+      </p>
     </div>
   );
 }
